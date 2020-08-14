bcea8b258ce91775867ff10a7eb4faae04dda0bf
mario <mariovishal12@gmail.com>
2020-08-14 22:17:20 +0530

    permutation

---
+++ StringPermutation.py
@@ -0,0 +1,25 @@
+def permutate(s):
+
+    out = []
+    #Base Case ----
+    if len(s) ==1:
+        out = [s]
+
+    else:
+
+        #for every letter in string 
+
+        for i,letter in  enumerate(s):
+
+            #for every permutation resulting fro step 2 and 3
+
+            for p in permutate(s[:i] + s[i+1:]):
+                # print("permutation is : ",p)
+                # print("current letter is : ",letter)
+                
+                out += [letter+p]
+                # print("output is :",out)
+
+    return out
+
+print(permutate('abc'))
