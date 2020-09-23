d27d43367370d7a41faf6635077441c0ed409207
mario <mariovishal12@gmail.com>
2020-09-23 19:00:11 +0530

    amazon

---
+++ SubstringWithSimilarFirstAndLast.py
@@ -0,0 +1,48 @@
+def find(s):
+
+    if len(s)<2:
+        return len(s)
+    left=0
+    right = len(s)-1
+
+    dict={}
+
+    count=0
+
+    while left<right:
+       
+        if s[left] in dict.keys():
+            dict[s[left]]+=1
+        else:
+            dict[s[left]]=1
+            
+        if s[right] in dict.keys():
+            dict[s[right]]+=1
+           
+        else:
+            dict[s[right]]=1
+
+        left+=1
+        right-=1
+
+    if len(s)%2!=0:
+        if s[right] in dict.keys():
+            dict[s[right]]+=1
+        else:
+            dict[s[right]]=1
+
+
+    for i in dict.values():
+        count+=i*(i+1)//2
+    
+    print(count)
+
+
+s='mariovishal'
+find(s)
+
+        
+
+        
+
+        
